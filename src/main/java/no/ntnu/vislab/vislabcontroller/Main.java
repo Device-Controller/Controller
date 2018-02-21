505a098c50482d3a7f71158ce7451ae796c6a6b8
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-02-21 11:05:00 +0100

    FIXED 9 NULLABLE BUGS CUZ FUCK THIS SHAIT

--- src/main/java/no/ntnu/vislab/vislabcontroller/Main.java
+++ src/main/java/no/ntnu/vislab/vislabcontroller/Main.java
@@ -15,61 +15,59 @@ import no.ntnu.vislab.barkof22.CommunicationRunnable;
 public class Main {
 
     public static void main(String[] args) {
-
-        CommunicationRunnable cr = null;
         try {
-            cr = new CommunicationRunnable(InetAddress.getByName("158.38.65.45"), 1025);
+            CommunicationRunnable cr = new CommunicationRunnable(InetAddress.getByName("158.38.65.45"), 1025);
             Thread t1 = new Thread(cr);
             t1.start();
+            Scanner sc = new Scanner(System.in);
+            while (sc.hasNext()) {
+                String input = sc.nextLine();
+                switch (input) {
+                    case "poweron":
+                        cr.sendCommand(BarkoF22Command.powerOn());
+                        System.out.println("PÅ");
+                        break;
+                    case "poweroff":
+                        cr.sendCommand(BarkoF22Command.powerOff());
+                        System.out.println("AV");
+                        break;
+                    case "powerstate":
+                        cr.sendCommand(BarkoF22Command.powerState());
+                        break;
+                    case "settings":
+                        System.out.println("RIGHTY-O");
+                        //c.retrieveSettings();
+                        break;
+                    case "mute":
+                        cr.sendCommand(BarkoF22Command.mute());
+                        System.out.println("MUTED");
+                        break;
+                    case "unmute":
+                        cr.sendCommand(BarkoF22Command.unMute());
+                        break;
+                    case "lamptime1":
+                        cr.sendCommand(BarkoF22Command.lamp1Runtime());
+                        break;
+                    case "lamptime2":
+                        cr.sendCommand(BarkoF22Command.lamp2Runtime());
+                        break;
+                    case "lampstatus1":
+                        cr.sendCommand(BarkoF22Command.lamp1Status());
+                        break;
+                    case "lampstatus2":
+                        cr.sendCommand(BarkoF22Command.lamp2Status());
+                        break;
+                    case "quit":
+                        System.out.println("HADE");
+                        return;
+                    default:
+                        break;
+                }
+                sc = new Scanner(System.in);
+            }
+
         } catch (UnknownHostException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String input = sc.nextLine();
-            switch (input) {
-                case "poweron":
-                    cr.sendCommand(BarkoF22Command.powerOn());
-                    System.out.println("PÅ");
-                    break;
-                case "poweroff":
-                    cr.sendCommand(BarkoF22Command.powerOff());
-                    System.out.println("AV");
-                    break;
-                case "powerstate":
-                    cr.sendCommand(BarkoF22Command.powerState());
-                    break;
-                case "settings":
-                    System.out.println("RIGHTY-O");
-                    //c.retrieveSettings();
-                    break;
-                case "mute":
-                    cr.sendCommand(BarkoF22Command.mute());
-                    System.out.println("MUTED");
-                    break;
-                case "unmute":
-                    cr.sendCommand(BarkoF22Command.unMute());
-                    break;
-                case "lamptime1":
-                    cr.sendCommand(BarkoF22Command.lamp1Runtime());
-                    break;
-                case "lamptime2":
-                    cr.sendCommand(BarkoF22Command.lamp2Runtime());
-                    break;
-                case "lampstatus1":
-                    cr.sendCommand(BarkoF22Command.lamp1Status());
-                    break;
-                case "lampstatus2":
-                    cr.sendCommand(BarkoF22Command.lamp2Status());
-                    break;
-                case "quit":
-                    System.out.println("HADE");
-                    return;
-                default:
-                    break;
-            }
-            sc = new Scanner(System.in);
         }
     }
 }
