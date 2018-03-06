a8fca03444c589f18c204ab9b4332873a4c892f1
erikhny <erikhny@gmail.com>
2018-03-06 09:44:44 +0100

    JavaScript Clean-upish

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -9,48 +9,42 @@ class doStuff {
         var i;
         for (i = 0; i < this.status.length; i++) {
             console.log(this.status[i]);
-            var currentLi = this.status[i];
-            this.status[i].onclick = event => {
+            this.status[i].onclick = event =>
+            {
                 event.target.style.backgroundColor = "red";
                 fetch(/*'controller/lampStatus?lampNumber=1'*/'controller/testDummy')
-                        .then(response => {
-                            if (response.ok) {
-                                return response.text();
-                            }
+                    .then(response => {
+                    if(response.ok)
+                return response.text();
+                {
+                }
 
-                            throw new Error("Failed");
-                        }).then(data => {
+                throw new Error("Failed");
+            }).
+                then(data => {
                     console.log(data);
-                })
-                        .catch(e => console.log("Error: " + e.message));
-            };
+            })
+            .
+                catch(e => console.log("Error: " + e.message));};
+
+
+            this.worker = new Worker("worker.js");
+            this.worker.postMessage("rofl");
+
+            this.worker.addEventListener('message', function (e) {
+                console.log('MAIN THREAD' + e.data);
+                document.getElementById('pro1').style.backgroundColor = e.data;
+            }, false);
 
         }
-        function handleWorkerMessage(msg) {
-            console.log("Message received: " + msg);
-            this.projector = document.getElementById("pro1");
-            switch (msg) {
-                case 'red':
-                    this.projector.style.backgroundColor = "red"
-                    break;
-                case 'blue':
-                    this.projector.style.backgroundColor = "blue"
-                    break;
-            }
-        }
-
-        this.worker = new Worker("worker.js");
-        this.worker.postMessage("rofl");
-
-        this.worker.addEventListener('message', function(e) {
-            console.log('MAIN THREAD' + e.data);
-            document.getElementById('pro1').style.backgroundColor = e.data;
-        }, false);
-
     }
 }
+
 let script = new doStuff();
-/*function getLampStatus() {
+
+
+
+function getLampStatus() {
     fetch('controller/lampStatus?lampNumber=1')
             .then(response => {
                 if (response.ok) {
@@ -63,8 +57,5 @@ let script = new doStuff();
     })
             .catch(e => console.log("Error: " + e.message));
 }
-function clickStuff() {
-    getLampStatus();
-}*/
 
 
