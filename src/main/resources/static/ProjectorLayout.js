0912acfb1ecd7ff23ff097ddaf5586d246500e43
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-03-14 13:31:15 +0100

    asdasd

--- src/main/resources/static/ProjectorLayout.js
+++ src/main/resources/static/ProjectorLayout.js
@@ -1,28 +1,17 @@
-
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /*exit the function:*/
-            return;
-        }
-    }
-}
+var myCanvas = document.getElementById('projector-layout');
+var ctx = myCanvas.getContext('2d');
+var tempCanvas = document.createElement('canvas')
+tempCanvas.style.width = "128px";
+tempCanvas.style.height = "128px";
+tempCanvas.style.background = "#FFFFFF";
+var tempCtx = tempCanvas.getContext('2d');
+var img = new Image;
+img.onload = function(){
+    tempCtx.save();
+    tempCtx.rotate(45*Math.PI/180);
+    tempCtx.drawImage(img, 64,0,128,128);
+    tempCtx.restore();
+    ctx.drawImage(tempCanvas,0,0); // Or at whatever offset you like
+};
+img.src = "power.png";
+img.style.transform = "rotate(90deg)";
