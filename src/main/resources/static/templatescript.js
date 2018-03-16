da5ca5314a85f671a814ee400c29262bec909799
erikhny <erikhny@gmail.com>
2018-03-16 12:40:09 +0100

    Improved CSS. Implemented JavaScript function that includes a html-file inside an html. Created useradmin.html Created template.html Navigation-menu now works properly on mobile.

---
+++ src/main/resources/static/templatescript.js
@@ -0,0 +1,37 @@
+/*
+ * includeHTML code is borrowed from https://www.w3schools.com/howto/howto_html_include.asp 
+ */
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
+function dropDownFunction() {
+    var x = document.getElementById("navMob");
+    if (x.className === "nav-mob") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-mob";
+    }
+}
