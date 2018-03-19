728b1dec6d80cdbe37b972b682b9807bf4fd51b8
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-03-19 11:30:56 +0100

    projector specific page now hooked up to actual projector baseline has been set.

--- src/main/java/no/ntnu/vislab/vislabcontroller/webcontroller/DeviceController.java
+++ src/main/java/no/ntnu/vislab/vislabcontroller/webcontroller/DeviceController.java
@@ -3,42 +3,17 @@ package no.ntnu.vislab.vislabcontroller.webcontroller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.net.InetAddress;
 import java.util.List;
 
-import no.ntnu.vislab.barkof22.BarkoF22Interface;
-import no.ntnu.vislab.barkof22.BarkoF22InterfaceImpl;
 import no.ntnu.vislab.vislabcontroller.DummyBase.Device;
 import no.ntnu.vislab.vislabcontroller.DummyBase.DummyBase;
 
 @Controller
 @RequestMapping("/controller")
 public class DeviceController {
-    private static BarkoF22Interface projector;
-
-    @RequestMapping("/lampStatus")
-    public ResponseEntity<Integer> status(@RequestParam(value = "lampNumber", required = false, defaultValue = "1") int lampNumber, Model model) throws Exception {
-        model.addAttribute("lampNumber", lampNumber);
-        if(projector == null){
-            projector = new BarkoF22InterfaceImpl("MONG Ltd", "ERIK", InetAddress.getByName("158.38.101.110"), 1025);
-        }
-        Integer response = projector.getLampStatus(1);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-    @RequestMapping("/lampRuntime")
-    public ResponseEntity<Integer> runtime(@RequestParam(value = "lampNumber", required = false, defaultValue = "World") int lampNumber, Model model) throws Exception {
-        model.addAttribute("lampNumber", lampNumber);
-        if(projector == null){
-            projector = new BarkoF22InterfaceImpl("MONG Ltd", "ERIK", InetAddress.getByName("158.38.101.110"), 1025);
-        }
-        int response= projector.getLampRuntime(1);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
     @RequestMapping("/testDummy")
     public ResponseEntity<String> dummy() {
         long time = System.currentTimeMillis();
--- src/main/resources/static/Projector/projector.js
+++ src/main/resources/static/Projector/projector.js
@@ -4,14 +4,14 @@ let id;
 function parseURLId(url) {
     let startIndex = url.indexOf("?") + 1;
     let endIndex = url.length + 1;
-    parseId(url.slice(startIndex, endIndex - 1));
+    return parseId(url.slice(startIndex, endIndex - 1));
 
 }
 
 function parseId(id) {
     let startIndex = id.indexOf("=") + 1;
     let endIndex = id.length + 1;
-    fetchProjector(id.slice(startIndex, endIndex - 1));
+    return id.slice(startIndex, endIndex - 1);
 }
 
 function fetchProjector(id) {
@@ -21,4 +21,20 @@ function fetchProjector(id) {
             response.json().then(p => console.log(p));
         }
     });
+}
+
+function mute() {
+    fetch('BarkoF22/mute?id=' + parseURLId(location.href)).then(response => {
+        if (response.ok) {
+            response.json().then(e => console.log(e));
+        }
+    })
+}
+
+function unMute() {
+    fetch('BarkoF22/unMute?id=' + parseURLId(location.href)).then(response => {
+        if (response.ok) {
+            response.json().then(e => console.log(e));
+        }
+    })
 }
