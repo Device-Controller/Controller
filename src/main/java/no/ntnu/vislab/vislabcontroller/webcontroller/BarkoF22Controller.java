3e52706c97cf38bc324e4b81b1803c2efe36eef1
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-03-20 11:58:18 +0100

    bug fix for thread generation

--- src/main/java/no/ntnu/vislab/vislabcontroller/webcontroller/BarkoF22Controller.java
+++ src/main/java/no/ntnu/vislab/vislabcontroller/webcontroller/BarkoF22Controller.java
@@ -18,42 +18,50 @@ import no.ntnu.vislab.vislabcontroller.DummyBase.DummyBase;
 @RequestMapping("/BarkoF22")
 public class BarkoF22Controller {
     private HashMap<Integer, BarkoF22Projector> activeProjectors;
+
     @RequestMapping("/mute")
-    public ResponseEntity<Integer>muteImage(@RequestParam (value = "id") int id) throws IOException {
+    public ResponseEntity<Integer> muteImage(@RequestParam(value = "id") int id) throws IOException {
         BarkoF22Projector projector = getProjector(id);
         return new ResponseEntity<>(projector.mute(), HttpStatus.OK);
     }
+
     @RequestMapping("/unMute")
-    public ResponseEntity<Integer>unMuteImage(@RequestParam (value = "id") int id) throws IOException {
+    public ResponseEntity<Integer> unMuteImage(@RequestParam(value = "id") int id) throws IOException {
         BarkoF22Projector projector = getProjector(id);
         return new ResponseEntity<>(projector.unMute(), HttpStatus.OK);
     }
 
     @RequestMapping("/powerState")
-    public ResponseEntity<String> getPowerStatus(@RequestParam (value = "id")int id) throws IOException {
+    public ResponseEntity<String> getPowerStatus(@RequestParam(value = "id") int id) throws IOException {
         BarkoF22Projector projector = getProjector(id);
         return new ResponseEntity<>(projector.getPowerState() + "", HttpStatus.OK);
     }
 
     @RequestMapping("/lampStatus")
-    public ResponseEntity<String> getLampStatus(@RequestParam (value = "id")int id, @RequestParam (value = "lampNum") int lampNum) throws IOException {
+    public ResponseEntity<String> getLampStatus(@RequestParam(value = "id") int id, @RequestParam(value = "lampNum") int lampNum) throws IOException {
         BarkoF22Projector projector = getProjector(id);
         return new ResponseEntity<>(projector.getLampStatus(lampNum) + "", HttpStatus.OK);
     }
 
     /**
      * Returns an active projector. If the projector is not active it starts it and adds it to a list.
+     *
      * @param id the id of the projector
      * @return the active projector, given by the id
      * @throws IOException if we dont have access to the port.
      */
     private BarkoF22Projector getProjector(int id) throws IOException {
-        if(activeProjectors == null){
+        if (activeProjectors == null) {
             activeProjectors = new HashMap<>();
         }
-        Device device = new DummyBase().getSingle(id);
-        BarkoF22Projector projector = new BarkoF22Projector(InetAddress.getByName(device.getIp()), device.getPort());
-        activeProjectors.put(device.getId(), projector);
+        BarkoF22Projector projector;
+        if (!activeProjectors.keySet().contains(id)) {
+            Device device = new DummyBase().getSingle(id);
+            projector = new BarkoF22Projector(InetAddress.getByName(device.getIp()), device.getPort());
+            activeProjectors.put(device.getId(), projector);
+        } else {
+            projector = activeProjectors.get(id);
+        }
         return projector;
     }
 }
