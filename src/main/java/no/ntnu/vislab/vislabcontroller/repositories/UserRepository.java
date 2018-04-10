20354c7ed014a502841109d6b60c0a5c90852100
ThomasSTodal <ThomaTod@stud.ntnu.no>
2018-04-10 12:32:37 +0200

    trying to fix build errors

--- src/main/java/no/ntnu/vislab/vislabcontroller/repositories/UserRepository.java
+++ src/main/java/no/ntnu/vislab/vislabcontroller/repositories/UserRepository.java
@@ -1,10 +1,7 @@
 package no.ntnu.vislab.vislabcontroller.repositories;
 
 import no.ntnu.vislab.vislabcontroller.entity.User;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 }
