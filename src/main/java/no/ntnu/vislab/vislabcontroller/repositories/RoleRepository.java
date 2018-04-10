283d0083796b6bfe6425d72f84dbc2b1015a492e
ThomasSTodal <ThomaTod@stud.ntnu.no>
2018-04-10 11:47:52 +0200

    Added the rest of the repositories

---
+++ src/main/java/no/ntnu/vislab/vislabcontroller/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package no.ntnu.vislab.vislabcontroller.repositories;
+
+import no.ntnu.vislab.vislabcontroller.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+}
--- src/main/java/no/ntnu/vislab/vislabcontroller/webcontroller/DeviceController.java
+++ src/main/java/no/ntnu/vislab/vislabcontroller/webcontroller/DeviceController.java
@@ -2,16 +2,26 @@ package no.ntnu.vislab.vislabcontroller.webcontroller;
 
 import no.ntnu.vislab.vislabcontroller.dummybase.DummyBase;
 import no.ntnu.vislab.vislabcontroller.dummybase.DummyDevice;
+import no.ntnu.vislab.vislabcontroller.entity.User;
+import no.ntnu.vislab.vislabcontroller.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/test")
 public class DeviceController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/testDummy")
     public ResponseEntity<String> dummy() {
         long time = System.currentTimeMillis();
@@ -23,8 +33,27 @@ public class DeviceController {
         }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
     @RequestMapping("/db")
     public ResponseEntity<List<DummyDevice>> dummyDevices(){
         return new ResponseEntity<>(new DummyBase().getList(), HttpStatus.OK);
+    }
+
+    @GetMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestParam String name
+                                            , @RequestParam String password
+                                            , @RequestParam String email) {
+        User n = new User();
+        n.setUsername(name);
+        n.setPassword(password);
+        n.setEmail(email);
+        userRepository.save(n);
+
+        return "saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
