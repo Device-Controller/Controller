05363d15752a0cff59d966108dc526b81de226bf
ThomasSTodal <ThomaTod@stud.ntnu.no>
2018-04-10 13:32:39 +0200

    continued attempts..

--- src/main/java/no/ntnu/vislab/vislabcontroller/repositories/UserRepository.java
+++ src/main/java/no/ntnu/vislab/vislabcontroller/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package no.ntnu.vislab.vislabcontroller.repositories;
 
 import no.ntnu.vislab.vislabcontroller.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
