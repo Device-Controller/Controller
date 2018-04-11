812ba40d15fe00b34c581b4317edae630b5e8eeb
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-04-11 11:03:50 +0200

    Made @DeviceSPI annotation for devices that are not projectors

--- src/main/java/no/ntnu/vislab/vislabcontroller/annotations/processors/DeviceAnnotationProcessor.java
+++ src/main/java/no/ntnu/vislab/vislabcontroller/annotations/processors/DeviceAnnotationProcessor.java
@@ -9,17 +9,17 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 
-import no.ntnu.vislab.vislabcontroller.annotations.ProjectorSPI;
+import no.ntnu.vislab.vislabcontroller.annotations.DeviceSPI;
 
 @SuppressWarnings({"Since15"})
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-@SupportedAnnotationTypes("no.ntnu.vislab.vislabcontroller.annotations.ProjectorSPI")
-public class ProjectorAnnotationProcessor extends AbstractProcessor {
+@SupportedAnnotationTypes("no.ntnu.vislab.vislabcontroller.annotations.DeviceSPI")
+public class DeviceAnnotationProcessor extends AbstractProcessor {
     private static final String PATH = "META-INF/services/";
-    private static final String CONTRACT = "no.ntnu.vislab.vislabcontroller.providers.Projector";
+    private static final String CONTRACT = "no.ntnu.vislab.vislabcontroller.providers.Device";
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return AnnotationProccesor.proccess(processingEnv,roundEnv, ProjectorSPI.class,CONTRACT,PATH);
+        return AnnotationProccesor.proccess(processingEnv,roundEnv, DeviceSPI.class,CONTRACT,PATH);
     }
 }
