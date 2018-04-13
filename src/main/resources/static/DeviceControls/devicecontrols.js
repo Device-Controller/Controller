07315a693167180fdcd0ace310a3b6669579cbdb
erikhny <erikhny@gmail.com>
2018-04-13 11:13:38 +0200

    Implemented update function for the device tables on the general device page. It does not work yet, but doesn't cause any errors except for invalid URL.

--- src/main/resources/static/DeviceControls/devicecontrols.js
+++ src/main/resources/static/DeviceControls/devicecontrols.js
@@ -124,8 +124,74 @@ function loadDevices() {
         console.log(table);
         console.log(document.body);
         document.body.appendChild(table);
+        updateData(numberOfDevices);
 
     }
+}
+
+function updateData(id) {
+    //GET CONTRAST
+    fetch('../BarkoF22/getContrast?id=' + id).then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("get-contrast").innerHTML = e);
+        }
+    });
+
+//GET POWERSTATE
+    fetch('../BarkoF22/powerState?id=' + id).then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("get-powerstate").innerHTML = e);
+        }
+    });
+
+//GET BRIGHTNESS
+    fetch('../BarkoF22/getBrightness?id=' + id).then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("brightness").innerHTML = e);
+        }
+    });
+
+//GET TEMPERATURE
+    fetch('../BarkoF22/getThermal?id=' + id).then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("temperature").innerHTML = e);
+        }
+    });
+
+//GET PROJECTOR RUNTIME
+    fetch('../BarkoF22/getProjectorRuntime?id=' + id).then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("projector-runtime").innerHTML = e);
+        }
+    });
+
+//GET LAMPRUNTIME 1
+    fetch('../BarkoF22/lampRuntime?id=' + id + '&lampNum=1').then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("lamp1-runtime").innerHTML = e);
+        }
+    });
+
+//GET LAMPSTATUS 1
+    fetch('../BarkoF22/lampStatus?id=' + id + '&lampNum=1').then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("lamp1-status").innerHTML = e);
+        }
+    });
+
+//GET LAMPRUNTIME 2
+    fetch('../BarkoF22/lampRuntime?id=' + id + '&lampNum=2').then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("lamp2-runtime").innerHTML = e);
+        }
+    });
+
+//GET LAMPSTATUS 2
+    fetch('../BarkoF22/lampStatus?id=' + id + '&lampNum=2').then(response => {
+        if (response.ok) {
+            response.json().then(e => document.getElementById("lamp2-status").innerHTML = e);
+        }
+    });
 }
 
 
