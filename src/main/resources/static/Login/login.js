65ce8f542903606d0518bb6c1f9854346e980479
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-04-20 11:56:27 +0200

    login js fix

--- src/main/resources/static/Login/login.js
+++ src/main/resources/static/Login/login.js
@@ -5,7 +5,7 @@ function login(){
     console.log(password);
     fetch('login/login?username=' + username + '&password=' + password).then(r=>{
         if(r.ok){
-            r.json().then(e => console.log(e));
+            r.json().then(e=>window.location.replace(window.location.origin + e.link));
         }
     })
 }
